689647210ec1ed0301a187eb25dfe20eaf525b47
Guilherme Dias <gmd.delta@gmail.com>
2021-12-12 20:08:38 -0300

    [PT-BR] Criada tela basica main de navegaçao de usuario. [EN] Created basic main screen for user navegation.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Redirect } from 'react-router';
 import '../css/pages/Main.css';
 
 function Main() {
+  const [redirect, setRedirect] = useState(null);
 
   return (
     <>
-      <p> Hi i'am Main</p>
-    </>  
+      {redirect && <Redirect to={redirect} />}
+      <header className='mainHeader'>
+        <h1>Main</h1>
+        <button onClick={() => setRedirect('/userConfig')}> config </button>
+      </header>
+      <main>
+        <button onClick={() => setRedirect('/student') }>Student</button>
+        <button onClick={() => setRedirect('/mentor') }>Mentor</button>
+        <button onClick={() => setRedirect('/tips') }>Tips</button>
+      </main>
+    </>
   );
 }
 
