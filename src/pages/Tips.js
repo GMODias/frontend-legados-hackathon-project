c5d73a53d98bb6748800a5cffa51c4e9a5f86994
marlon307 <marlon-ramosb@hotmail.com>
2021-12-13 17:17:56 -0300

    Create card tips options

--- src/pages/Tips.js
+++ src/pages/Tips.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import { ButtonWithImage, ThemeSelector } from '../components/index';
+import Linux from '../components/tips/Linux';
 import '../css/pages/Tips.css';
 
 function Tips() {
   const [selectedTheme, setSelectedTheme] = useState(null);
   return (
-    <>
-      <ButtonWithImage kind="main" />
-      { selectedTheme && <ButtonWithImage kind="back" />}
-      { !selectedTheme && <ThemeSelector setTheme={ setSelectedTheme } /> }
-      { selectedTheme && `vou te mostrar as dicas de ${selectedTheme}` }
-    </>
+    <div className="tips">
+      <div className="header">
+        <ButtonWithImage kind="main" />
+        { selectedTheme && <ButtonWithImage kind="back" /> }
+      </div>
+      <div className="btntipes">
+        { !selectedTheme && <ThemeSelector setTheme={ setSelectedTheme } /> }
+      </div>
+
+      { selectedTheme && `Vou te mostrar as dicas de ${selectedTheme}` }
+      { selectedTheme === 'Linux' && <Linux /> }
+      { selectedTheme === 'Linux' && <Linux /> }
+      { selectedTheme === 'Linux' && <Linux /> }
+      { selectedTheme === 'Linux' && <Linux /> }
+    </div>
   );
 }
 
