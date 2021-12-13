39b528017bdc3b61ba1fc5346e2eb0f5c30ec461
Guilherme Dias <gmd.delta@gmail.com>
2021-12-13 00:24:46 -0300

    [PT-BR] Resolvido problemas de Lint. [EN] Solved Lint problems.

--- src/components/Table.js
+++ src/components/Table.js
@@ -44,7 +44,7 @@ function Table() {
     const dez = 10;
     if (num < dez) {
       const string = String(num);
-      return '0' + string;
+      return `0${string}`;
     }
     return num;
   };
@@ -52,73 +52,89 @@ function Table() {
   const makeDateInfo = () => {
     const newdateInfo = [];
     const todayDate = new Date();
+    const safetyTime = 12;
+    const qtdCalendarDay = 7;
+    setTimeNow(`${lenTwo(todayDate.getHours() + safetyTime)}
+      :${lenTwo(todayDate.getMinutes())}`);
 
-    setTimeNow(`${lenTwo(todayDate.getHours() + 12)}:${lenTwo(todayDate.getMinutes())}`);
-
-    for (let index = 0; index < 7; index += 1) {
+    for (let index = 0; index < qtdCalendarDay; index += 1) {
       newdateInfo
         .push({ monthDay: todayDate
           .toLocaleDateString(),
         weekDay: weekDays[todayDate.getDay()] });
-      if (index < 6) todayDate.setDate(todayDate.getDate() + 1);
+      if (index < qtdCalendarDay - 1) todayDate.setDate(todayDate.getDate() + 1);
     }
     setDateInfo(newdateInfo);
   };
 
   const removeDay = (time) => {
+    const hoursInDay = 24;
     const timeData = time.split(':');
-    const dayDifference = Number(timeData[0]) - 24;
-    const removedTime = dayDifference > 0 ? `${lenTwo(dayDifference)}:${timeData[1]}` : '00:00';
+    const dayDifference = Number(timeData[0]) - hoursInDay;
+    const removedTime = dayDifference > 0 ? `${lenTwo(dayDifference)}:${timeData[1]}`
+      : '00:00';
     return removedTime;
   };
 
   const btnTimeClick = ({ target }) => {
     const scheduleClickAction = {
-      'greenlane': () => {
+      greenlane: () => {
         setDataToSend(dataToSend.filter((e) => e !== target.id));
         target.className = `${cellBaseClName} emptybox`;
       },
-      'emptybox': () => {
+      emptybox: () => {
         setDataToSend([...dataToSend, target.id]);
         target.className = `${cellBaseClName} greenlane`;
       },
       // Confirmed deverá abrir um modal para mostrar os dados do mentor, aluno, ok para sair e cancelar.
-      'confirmed': () => 1,
+      confirmed: () => 1,
       // Ao clicar no avaible e depois salvar marcações (botão a ser criado, ele deve enviar para o backend a marcação e atualizar os dados na tela).
-      'avaiable': () => 1,
-      'blocked': () => 1,
+      avaiable: () => 1,
+      blocked: () => 1,
     };
     scheduleClickAction[target.classList[1]]();
   };
 
-  const cellDate = (i, j, scheduleTimes, _tableHeader) => {
+  const cellDate = (i, j, scheduleTimes) => {
+    const bit2 = 2;
+    const bit3 = 4;
+    const bit4 = 8;
+    const bit5 = 16;
     if (j === 0) return scheduleTimes[i];
-    const id = JSON.stringify({ time: scheduleTimes[i], date: dateInfo[j - 1].monthDay });
-    const cellStatus = 2 + confirmedMock.includes(id) * 4 + teachingAvaiabilityMock.includes(id) * 8
-      + ((timeNow > scheduleTimes[i] && j === 1)
-        || (removeDay(timeNow) > scheduleTimes[i] && j === 2)) * 16;
+    const id = JSON.stringify({ time: scheduleTimes[i],
+      date: dateInfo[j - 1].monthDay });
+    const cellStatus = bit2 + confirmedMock.includes(id) * bit3
+      + teachingAvaiabilityMock.includes(id) * bit4
+        + ((timeNow > scheduleTimes[i] && j === 1)
+          || (removeDay(timeNow) > scheduleTimes[i] && j === 2)) * bit5;
     return (
       <button
-        type='button'
-        id={id}
-        className={`${cellBaseClName} ${tableCellClasses[parseInt(Math
-          .log2(cellStatus, 10))]}`}
-        onClick={btnTimeClick}
-      ></button>
+        type="button"
+        id={ id }
+        aria-label="s"
+        className={ `${cellBaseClName} ${tableCellClasses[parseInt(Math
+          .log2(cellStatus), 10)]}` }
+        onClick={ btnTimeClick }
+      />
     );
   };
 
   const makeDataTable = () => {
-    const tableHeader = dateInfo.map((e) => `${e.monthDay.split('/20')[0]} \n\n ${e.weekDay}`);
+    const halfHour = 30;
+    const tableHeader = dateInfo.map((e) => `${e.monthDay.split('/20')[0]} 
+      \n\n ${e.weekDay}`);
+
     tableHeader.unshift('Horários');
-    const scheduleTimes = Array.from({ length: 48 }, (_, i) =>
-      `${lenTwo(parseInt(i / 2))}:${lenTwo((i % 2) * 30)} `);
+
+    const scheduleTimes = Array.from({ length: 48 },
+      (_, i) => `${lenTwo(parseInt(i / 2, 10))}:${lenTwo((i % 2) * halfHour)} `);
+
     return (
       <div className="table-container">
         <table>
           <thead>
             <tr>
-              {tableHeader.map((head, i) => <th key={i}>{head}</th>)}
+              { tableHeader.map((head, i) => <th key={ i }>{head}</th>) }
             </tr>
           </thead>
           <tbody>
@@ -126,9 +142,9 @@ function Table() {
               <tr key={ i }>
                 {tableHeader.map((_, j) => (
                   <td
-                    key={String(i) + String(j)}
+                    key={ String(i) + String(j) }
                   >
-                    {cellDate(i, j, scheduleTimes, tableHeader)}
+                    {cellDate(i, j, scheduleTimes)}
                   </td>))}
               </tr>
             ))}
