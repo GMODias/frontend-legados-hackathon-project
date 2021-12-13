712ccdf9a631418963ccbcb2c19e3d0ac1db02ad
Guilherme Dias <gmd.delta@gmail.com>
2021-12-13 18:26:33 -0300

    [PT-BR] Adicionado Tela Mentor e alterado logica da tela Estudante . [EN] Added Mentor Screen and changed Student screen logic.

--- src/pages/MentorSchedule.js
+++ src/pages/MentorSchedule.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { ButtonWithImage } from '../components/index';
+import React, { useState } from 'react';
+import { Modal, Table, ButtonWithImage } from '../components';
 import '../css/pages/MentorSchedule.css';
 
 function MentorSchedule() {
+  const [showModal, setShowModal] = useState(false);
+
+  const showTable = () => (
+    <div className="scheduleTableDiv">
+      <Table />
+    </div>
+  );
+
   return (
     <>
+      {showModal && <Modal props={ { setShowModal } } />}
       <ButtonWithImage kind="main" />
-      <p> Hi i am MentorSchedule</p>
+      <button type="button" onClick={ () => setShowModal(true) }>teste</button>
+      <h1>Marcação de Mentorias - MENTOR</h1>
+      { showTable() }
+
     </>
   );
 }
--- src/pages/StudentSchedule.js
+++ src/pages/StudentSchedule.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
-import { Modal, Table, ButtonWithImage } from '../components';
+import { Modal, Table, ButtonWithImage, ThemeSelector } from '../components';
 import '../css/pages/StudentSchedule.css';
 
 function StudentSchedule() {
   const [showModal, setShowModal] = useState(false);
+  const [selectedTheme, setSelectedTheme] = useState(null);
+
+  const showTable = () => (
+    <div className="scheduleTableDiv">
+      <Table />
+    </div>
+  );
+
   return (
     <>
       {showModal && <Modal props={ { setShowModal } } />}
       <ButtonWithImage kind="main" />
       <button type="button" onClick={ () => setShowModal(true) }>teste</button>
-      <div className="scheduleTableDiv">
-        <Table />
-      </div>
+      { selectedTheme && <ButtonWithImage kind="back" />}
+      { !selectedTheme && <ThemeSelector setTheme={ setSelectedTheme } /> }
+      { selectedTheme && `Marcação das Mentorias de ${selectedTheme}` }
+      { selectedTheme && showTable() }
+
     </>
   );
 }
